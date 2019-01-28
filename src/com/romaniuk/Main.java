63a529d137ccb38e108bdd68e2e14b2140f5c390
PawelRomaniuk <pawel.romaniuk@autodesk.com>
2019-01-28 14:09:18 +0100

    SQLJDBCJAVA - SQL JDBC datasource class

--- src/com/romaniuk/Main.java
+++ src/com/romaniuk/Main.java
@@ -1,69 +1,49 @@
 package com.romaniuk;
 
+import com.romaniuk.model.Datasource;
+
 import java.sql.*;
 
 public class Main {
-    private static final String DB_NAME = "javadb";
-    private static final String CONNECTION_STRING ="jdbc:mysql://localhost:3306/"+DB_NAME+"?autoReconnect=true&useSSL=false";
-    private static final String USER="root";
-    private static final String PASSWORD="Sasanka01";
-    private static final String TABLE_CONTACTS = "contacts";
-    private static final String COLUMN_NAME="name";
-    private static final String COLUMN_PHONE="phone";
-    private static final String COLUMN_EMAIL="email";
+
 
     public static void main(String[] args){
         // write your code here
 
-        String user = "root";
-        String password = "Sasanka01";
-        try {
-            Connection myConn = DriverManager.getConnection(CONNECTION_STRING,USER,PASSWORD);
-            Statement statement = myConn.createStatement();
+        Datasource datasource = new Datasource();
 
-            statement.execute("DROP TABLE IF EXISTS " + TABLE_CONTACTS);
-            statement.execute("CREATE TABLE IF NOT EXISTS " + TABLE_CONTACTS +
-                                    "(" + COLUMN_NAME+" text, " +
-                                            COLUMN_PHONE+" integer, " +
-                                            COLUMN_EMAIL + " text" +
-                                    ")");
-            // with method insertContactsStatement - reduction of duplicated code
-
-            insertContactsStatement(statement,"Pawel",48548755,"email@Email");
-            insertContactsStatement(statement,"John",48548755,"email@Email");
-            insertContactsStatement(statement,"Mark",48548755,"email@Email");
-
-            statement.close();
-            myConn.close();
-
-        } catch (SQLException exc) {
-            System.out.println("Something went wrong "+ exc.getMessage());
-            exc.printStackTrace();
+        if(!datasource.open()){
+            System.out.println("Can not open datasource");
+            return;
         }
-    }
+        datasource.close();
 
-    private static void close(Connection myConn, Statement myStmt,
-                              ResultSet myRs) throws SQLException {
-        if (myRs != null) {
-            myRs.close();
-        }
 
-        if (myStmt != null) {
-            myStmt.close();
-        }
+//        try {
+//            Connection myConn = DriverManager.getConnection(CONNECTION_STRING,USER,PASSWORD);
+//            Statement statement = myConn.createStatement();
+//
+//            statement.execute("DROP TABLE IF EXISTS " + TABLE_CONTACTS);
+//            statement.execute("CREATE TABLE IF NOT EXISTS " + TABLE_CONTACTS +
+//                                    "(" + COLUMN_NAME+" text, " +
+//                                            COLUMN_PHONE+" integer, " +
+//                                            COLUMN_EMAIL + " text" +
+//                                    ")");
+//            // with method insertContactsStatement - reduction of duplicated code
+//
+//            insertContactsStatement(statement,"Pawel",48548755,"email@Email");
+//            insertContactsStatement(statement,"John",48548755,"email@Email");
+//            insertContactsStatement(statement,"Mark",48548755,"email@Email");
+//
+//            statement.close();
+//            myConn.close();
+//
+//        } catch (SQLException exc) {
+//            System.out.println("Something went wrong "+ exc.getMessage());
+//            exc.printStackTrace();
+//        }
+}
 
-        if (myConn != null) {
-            myConn.close();
-        }
-    }
 
-    private static void insertContactsStatement (Statement statement, String name, int phone, String email) throws SQLException {
-        statement.execute("INSERT INTO " + TABLE_CONTACTS +
-                " (" + COLUMN_NAME + ", " +
-                COLUMN_PHONE + ", " +
-                COLUMN_EMAIL +
-                " ) " +
-                "VALUES('" + name + "', " + phone + ", '" + email + "')");
-    }
 }
 
--- src/com/romaniuk/model/Datasource.java
+++ src/com/romaniuk/model/Datasource.java
@@ -1,6 +1,45 @@
 package com.romaniuk.model;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 public class Datasource {
 
+    private static final String DB_NAME = "javadb";
+    private static final String CONNECTION_STRING ="jdbc:mysql://localhost:3306/"+DB_NAME+"?autoReconnect=true&useSSL=false";
+    private static final String USER="root";
+    private static final String PASSWORD="Sasanka01";
+    private static final String TABLE_CONTACTS = "contacts";
+    private static final String COLUMN_NAME="name";
+    private static final String COLUMN_PHONE="phone";
+    private static final String COLUMN_EMAIL="email";
 
+    String user = "root";
+    String password = "Sasanka01";
+
+    private Connection myConn;
+
+    public boolean open(){
+        try {
+            Connection myConn = DriverManager.getConnection(CONNECTION_STRING,USER,PASSWORD);
+            //Statement statement = myConn.createStatement();
+            System.out.println("Connected");
+            return true;
+        }catch (SQLException e){
+            System.out.println("Could not connect to database" + e.getMessage());
+            return false;
+        }
+    }
+
+    public void close(){
+        try {
+            if (myConn!=null){
+                myConn.close();
+            }
+        }catch (SQLException e){
+            System.out.println("Could not close connection" + e.getMessage());
+        }
+    }
 }
